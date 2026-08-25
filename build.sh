6fcc98b8bb8dbe385a09d75ef3feddf5fca70a73
Gamevalentine <anhtsizoo00@gmail.com>
2026-08-25 12:40:04 +0700

    Load final verified Wiki corrections

--- build.sh
+++ build.sh
@@ -49,6 +49,9 @@ fi
 if ! grep -q 'wiki_verified_data_v145\.js' public/wiki.html; then
   sed -i 's#</body>#  <script defer src="/wiki_verified_data_v145.js?v=145"></script>\n</body>#' public/wiki.html
 fi
+if ! grep -q 'wiki_verified_fix_v146\.js' public/wiki.html; then
+  sed -i 's#</body>#  <script defer src="/wiki_verified_fix_v146.js?v=146"></script>\n</body>#' public/wiki.html
+fi
 
 sed -i "s#'updates','/updates'#'updates','/ban-cap-nhat'#" public/navigation_v124.js
 sed -i 's#route:"/updates"#route:"/ban-cap-nhat"#g' public/mobile_menu_v5.js
@@ -66,6 +69,7 @@ test -f public/wiki_missing_images_v141.js
 test -f public/wiki_clean_mobile_weapons_v142.js
 test -f public/wiki_weapon_detail_v144.js
 test -f public/wiki_verified_data_v145.js
+test -f public/wiki_verified_fix_v146.js
 test -f 'functions/api/[[path]].js'
 
 if grep -RIl --include='*.html' --include='*.css' --include='*.js' '\.vercel\.app' public | grep -q .; then
